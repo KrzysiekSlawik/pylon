b972dec19c2faeff4fcff8a8629fb6279f7223de
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2022-02-02 21:59:13 +0100

    clean up active_game_sessions

--- src/server/database/active_game_sessions.py
+++ src/server/database/active_game_sessions.py
@@ -22,28 +22,10 @@ class GameSessionsManager:
     def list_games(self) -> GameSession:
         return self.games
 
-    def connect(websocket: WebSocket, game_id: int, player_id: int):
-        pass
+    async def connect(self, websocket: WebSocket, game_id: int, player_id: int):
+        await self.games[game_id].connect(websocket, player_id)
 
     def handle_msg(self, websocket: WebSocket, game_id: int, player_id: int, msg: Msg):
         self.games[game_id].handle_msg(websocket, player_id, msg)
-
-    def make_move(self, game_id, turn):
-        self.games[game_id].put_turn(turn)
-        return self.games[game_id].get_turn_nowait()
-
-    async def get_next_turn(self, game_id):
-        return await self.games[game_id].get_turn()
-
-    async def join_game(self, game_id:int, player_id:int):
-        return await self.games[game_id].join_session(player_id)
-
-    async def join_any_game(self, player_id:int):
-        games = list(filter(lambda g: len(g._players) < 2, self.games))
-        if len(games) > 0:
-            return await self.join_game(games[0].game_id, player_id)
-        else:
-            game_id = self.new_game()
-            return await self.join_game(game_id, player_id)
 
 current_sessions = GameSessionsManager()
