8657aebe24b34a93b4137dbfeba1c50279be3a3a
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2022-02-02 21:51:15 +0100

    add update session state after player move, update database after gameover

--- src/server/database/game_session.py
+++ src/server/database/game_session.py
@@ -88,31 +88,45 @@ class GameSession:
             self._update_state(move)
 
     async def _end_game(self):
-        winner = self._turn % 2
+        winner_id = self._turn % 2
         await self._broadcast(
             GameOverMsg(
                 {
-                    'winner_id': self._players_ids[winner],
-                    'winner_name': self._players_names[winner],
-                    'winner_tokens': self._tokens[winner]
+                    'winner_id': self._players_ids[winner_id],
+                    'winner_name': self._players_names[winner_id],
+                    'winner_tokens': self._tokens[winner_id]
                 }
             )
         )
+        winner = await user_pydantic.from_queryset_single(Users.get(id=self._players_ids[winner_id])).dict()
+        loser = await user_pydantic.from_queryset_single(Users.get(id=self._players_ids[(winner_id + 1) % 2])).dict()
+        winner['wins'] += 1
+        loser['loses'] += 1
+        await Users.filter(id=self._players_ids[winner_id]).update(**winner)
+        await Users.filter(id=self._players_ids[(winner_id + 1) % 2]).update(**loser)
         for connection in self._connections:
             connection.close()
 
-    async def _update_state(self, move):
+    async def _update_state(self, move: Dict):
         self._turn += 1
         player = self._turn % 2
-        cost = 0
-        self._board = []  # TODO
+        cost = 0 if move['cat'] is 'move' else 1
+        put_level = move['level']
+        put_x = move['x']
+        put_y = move['y']
+        self._board[put_level][put_x][put_y] = player
+        if move['cat'] is 'move':
+            take_level = move['take_level']
+            take_x = move['take_x']
+            take_y = move['take_y']
+            self._board[take_level][take_x][take_y] = 0
         self._tokens[player] -= cost
         self._next_legal = self._legal()
         self._broadcast(self._state_msg())
         if len(self._next_legal) == 0:
             self._end_game()
 
-    def _is_authorized_for_move(self, websocket: WebSocket, player_id) -> bool:
+    def _is_authorized_for_move(self, websocket: WebSocket, player_id: int) -> bool:
         current_turn = self._turn + 1
         current_player = current_turn % 2
         return (
